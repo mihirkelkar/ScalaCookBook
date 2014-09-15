7358f9aec49d49b242ab074ad0e2c6d5fd5de6c7
mihirkelkar <kelkarmhr@gmail.com>
2014-09-14 20:40:53 -0400

    Adding the first example of the Scala Cook Book here

---
+++ Chapter_One/example_one.scala
@@ -0,0 +1,8 @@
+//The first and the only taske here in this example is to sho you that Scala actually leverages the Java Libraries and is built up on the JVM
+//The following line gives us an example of what class a string belongs to. 
+//IN scala everything , even variables are objects and all functions are methods
+//of various classes by default
+println("Hello, World".getClass)
+
+//Get the name of the given class for a string
+println("Hello World".getClass.getName)
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ScalaCookBook
 =============
 
 I try and finish the scala cook book in a week documenting the code exercises I practice and general notes here. 
+
+I will try and maintain an index here as to specific problems that we might come across during writing code and their solutions here. 
+
+I will also try and draw parallel from different programming language constructs like those in Python to the equivalent in Scala. 
